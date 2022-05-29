7ce2c2eb032f730691668f55943ce6281a71aecf
ariw96 <ariw96@gmail.com>
2022-05-29 18:07:31 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const importData = require('./db.json');
+let port = process.env.PORT || 3000;
+app .get('/', (req, res) => {
+    res.send('Hello World!');
+})
+app.get("/shoes", (req, res) => {
+    res.send(importData);
+});
+app.listen(port, () => {
+console.log(`Example app listening on port ${port}!`);
+});
+ 
