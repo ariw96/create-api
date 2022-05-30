c480872e8dcd9d7d6c11a7f295fd26e65336525c
ariw96 <ariw96@gmail.com>
2022-05-30 08:07:40 +0300

    update

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(cors());
+
 const importData = require('./db.json');
 let port = process.env.PORT || 3000;
 app .get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World update!');
 })
 app.get("/shoes", (req, res) => {
     res.send(importData);
